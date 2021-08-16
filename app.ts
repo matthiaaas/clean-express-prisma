96ea5af81770169a4cd81714683eedbb1e40352d
matthiaaas <33065597+matthiaaas@users.noreply.github.com>
2021-08-16 14:14:27 +0200

    add gzip compression middleware

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import helmet from "helmet"
 import express from "express"
+import compression from "compression"
 import createRouter from "express-file-routing"
 
 import config from "./config"
@@ -11,6 +12,7 @@ const app = express()
 app.use(express.json())
 
 app.use(helmet())
+app.use(compression())
 
 // creates file-based routing for /routes
 createRouter(app)
